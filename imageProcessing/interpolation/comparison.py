c8bfdddb449ccff0012603bf401c8bb3ee1f7dc4
jundoopop <jundolpop@gmail.com>
2023-07-27 21:38:42 +0900

    Bilinear Interpolation: - Resolved ZeroDivisionError:     -> Applied a linear interpolation correctly     -> 2x upsample, 0.5x downsample implemented

--- imageProcessing/interpolation/comparison.py
+++ imageProcessing/interpolation/comparison.py
@@ -3,46 +3,49 @@ import numpy as np
 import interLinear as il
 import interCubic as ic
 
-img = cv.imread("../source/resize1.jpg", cv.IMREAD_GRAYSCALE)
+img = cv.imread(
+    "/home/treeplanter/graphics/pyOpenCv/imageProcessing/source/resize1.jpg",
+    cv.IMREAD_GRAYSCALE,
+)
 if img is None:
     print("Can't read the image")
     exit(0)
 
-ratio = 2  # Resize ratio
-dsize = (int(len(img[0]) * ratio), int(len(img) * ratio))  # dsize[0] = width, dsize[1] = height
+ratio = 0.5  # Resize ratio
+dsize = (
+    int(len(img) * ratio),
+    int(len(img[0]) * ratio),
+) 
 
-# sketchbook = np.ndarray(shape=dsize, dtype=np.float32)  # Create a new array for custom interpolation
+sketchbook_weighted = np.zeros(
+    shape=dsize, dtype=np.float32
+)  # Create a new array for custom interpolation
 
-# row: len(img), col: len(img[0])
-# for i in range(len(img)):
-#     for j in range(len(img[0])):
-#         sketchbook[i, j] = il.bi_inter(i // 2, j // 2, img)  # Apply custom interpolation to the image
-sketchbook_weighted = np.array(
-    [[il.weighted_mean(i // ratio, j // ratio, img) for i in range(dsize[0])] for j in range(dsize[1])])
-sketchbook_convoluted = np.array(
-    [[il.bilinear_kernel(i // ratio, j // ratio, img) for i in range(dsize[0])] for j in range(dsize[1])])
-
-sketchbook_cubic_convolution = np.array(
-    [[ic.bicubic_kernel(i // ratio, j // ratio, img) for i in range(dsize[0])] for j in range(dsize[1])])
+for i in range(0, len(img), 2):
+    for j in range(0, len(img[0]), 2):
+        # print(f"Curent coordinate: ({i}, {j})")
+        # print(
+        #     f"Shape of the imaging bound: {np.shape(sketchbook_weighted[2 * i : 2 * i + int(ratio * 2), 2 * j : 2 * j + int(ratio * 2)])}"
+        # )
+        # print(
+        #     f"Shape of the interpolated range:\n{il.weighted_mean(img[i, j], img[i, j + 1], img[i + 1, j], img[i + 1, j + 1], ratio)}"
+        # )
+        sketchbook_weighted[
+            int(ratio * i) : int(ratio * i) + int(ratio * 2), int(ratio * j) : int(ratio * j) + int(ratio * 2)
+        ] = il.weighted_mean(
+            img[i, j], img[i, j + 1], img[i + 1, j], img[i + 1, j + 1], ratio
+        )
+# Get the result of custom interpolation
 
 while cv.waitKey(0) != ord("q"):
     cv.imshow("Original", img)
     # CV library bilinear interpolation
-    cv.imshow("CV library bilinear interpolation",
-              cv.resize(img, dsize, fx=0, fy=0, interpolation=cv.INTER_LINEAR))
 
-    # cv.imshow("CV library nearest neighbor interpolation",
-    #           cv.resize(img, dsize, fx=0, fy=0, interpolation=cv.INTER_NEAREST))
+    cv.imshow(
+        "Custom bilinear interpolation - Downsample 0.5x",
+        sketchbook_weighted.astype(np.uint8),
+    )
 
-    # Custom bilinear interpolation formula -> Weighted mean
-    cv.imshow("Custom bilinear interpolation - weighted mean",
-              sketchbook_weighted.astype(np.uint8))
-    # Custom bilinear interpolation formula -> 3 x 3 kernel
-    cv.imshow("Custom bilinear interpolation - convolution",
-              sketchbook_convoluted.astype(np.uint8))
-    # Custom bicubic interpolation formula -> 4 x 4 kernel
-    cv.imshow("Custom bicubic interpolation - kernel",
-              sketchbook_cubic_convolution.astype(np.uint8))
 
 cv.destroyAllWindows()
 exit(0)
--- imageProcessing/interpolation/interLinear.py
+++ imageProcessing/interpolation/interLinear.py
@@ -11,41 +11,132 @@ import numpy as np
 
 
 # Calculating bilinear interpolation pixel value
-def weighted_mean(x, y, src):
-    '''Interpolate (x,y) from values associated with four points.
+def weighted_mean(
+    point_left_up, point_right_up, point_left_down, point_right_down, scale
+):  # Get the pixel values of the four coordinates on the corner
+    """Interpolate (x,y) from values associated with four points.
     The four points are a list of four triplets: (x, y, value).
     The four points can be in any order. They should form a rectangle.
-    '''
+    """
 
-    # Prevent the index error
-    if x >= src.shape[1] - 1:
-        x = x - 1
-    if y >= src.shape[0] - 1:
-        y = y - 1
+    """
+    Bilinear Interpolation:
+    1. Choose four nearest pixels (2 x 2)
+    2. 1.5 upscale: make (2 x 2) to (3 x 3)
+    3. 2 upscale: make (2 x 2) to (4 x 4)
+    """
 
+    pixel = 0
+
+    if scale == 0.5:  # 50% downscale
+        pixel = np.mean(
+            [point_left_up, point_right_up, point_left_down, point_right_down]
+        )
+
+    elif scale == 1.5:  # 150% upscale
+        # Interpolate between the top two corners and the bottom two corners
+
+        # First Interpolation
+        point_center_down = np.mean([point_left_down, point_right_down])
+        point_center_up = np.mean([point_left_up, point_right_up])
+        point_left_center = np.mean([point_left_up, point_left_down])
+        point_right_center = np.mean([point_right_up, point_right_down])
+
+        # Second Interpolation
+        point_center_center = np.mean([point_center_down, point_center_up])
+
+        # Upscaled Result
+        pixel = np.array(
+            [
+                [point_left_up, point_center_up, point_right_up],  # First row
+                [
+                    point_left_center,
+                    point_center_center,
+                    point_right_center,
+                ],  # Second row
+                [point_left_down, point_center_down, point_right_down],
+            ]
+        )  # Third row
+
+    elif scale == 2:  # 200% upscale
+        # First Interpolation
+        # Get values of the four corners
+        point_0_1 = np.average(
+            [point_left_up, point_right_up], weights=[1, 2]
+        )  # (0, 1)
+        point_0_2 = np.average(
+            [point_left_up, point_right_up], weights=[2, 1]
+        )  # (0, 2)
+        point_1_0 = np.average(
+            [point_left_up, point_left_down], weights=[1, 2]
+        )  # (1, 0)
+        point_2_0 = np.average(
+            [point_left_up, point_left_down], weights=[2, 1]
+        )  # (2, 0)
+        point_3_1 = np.average(
+            [point_left_down, point_right_down], weights=[1, 2]
+        )  # (3, 1)
+        point_3_2 = np.average(
+            [point_left_down, point_right_down], weights=[2, 1]
+        )  # (3, 2)
+        point_1_3 = np.average(
+            [point_right_up, point_right_down], weights=[1, 2]
+        )  # (1, 3)
+        point_2_3 = np.average(
+            [point_right_up, point_right_down], weights=[2, 1]
+        )  # (2, 3)
+
+        # Second Interpolation
+        point_1_1 = np.average([point_0_1, point_3_1], weights=[1, 2])  # (1, 1)
+        point_1_2 = np.average([point_0_2, point_3_2], weights=[1, 2])  # (1, 2)
+        point_2_1 = np.average([point_0_1, point_3_1], weights=[2, 1])  # (2, 1)
+        point_2_2 = np.average([point_0_2, point_3_2], weights=[2, 1])  # (2, 2)
+
+        # Upscaled Result
+        pixel = np.array(
+            [
+                [point_left_up, point_0_1, point_0_2, point_right_up],  # First row
+                [point_1_0, point_1_1, point_1_2, point_1_3],  # Second row
+                [point_2_0, point_2_1, point_2_2, point_2_3],  # Third row
+                [point_left_down, point_3_1, point_3_2, point_right_down],  # Fourth row
+            ]
+        )  
+
+    return pixel  # Return the result of the interpolation
+
+
+def legacy(src, x, y):
     # Declare four points for interpolation
-    left_up, right_up, left_down, right_down = \
-        src[y, x], src[y, x + 1], src[y + 1, x], src[y + 1, x + 1]  # Set the values in the four corners from the image
+    left_up, right_up, left_down, right_down = (
+        src[y, x],
+        src[y, x + 1],
+        src[y + 1, x],
+        src[y + 1, x + 1],
+    )  # Set the values in the four corners from the image
 
     # Interpolate between the top two corners and the bottom two corners
     if left_up + right_up == 0:
         upper_avg = 0
     else:
-        upper_avg = np.average([left_up, right_up],
-                               weights=[left_up, right_up])  # Weighted average of the upper two corners
+        upper_avg = np.average(
+            [left_up, right_up], weights=[left_up, right_up]
+        )  # Weighted average of the upper two corners
 
-    if right_down + left_down == 0:  # If the value of the upper two corners or the lower two corners is 0
+    if (
+        right_down + left_down == 0
+    ):  # If the value of the upper two corners or the lower two corners is 0
         lower_avg = 0
     else:
-        lower_avg = np.average([left_down, right_down],
-                               weights=[left_down, right_down])  # Weighted average of the lower two corners
+        lower_avg = np.average(
+            [left_down, right_down], weights=[left_down, right_down]
+        )  # Weighted average of the lower two corners
 
-    # Return the result of the interpolation
-    if upper_avg + lower_avg == 0:
-        return 0
-    else:
-        return np.clip(np.average([upper_avg, lower_avg],
-                                  weights=[upper_avg, lower_avg]), 0, 255)  # Weighted average of the two averages
+    # # Return the result of the interpolation
+    # if upper_avg + lower_avg == 0:
+    #     return 0
+    # else:
+    #     return np.clip(np.average([upper_avg, lower_avg],
+    #                               weights=[upper_avg, lower_avg]), 0, 255)  # Weighted average of the two averages
 
 
 def bilinear_kernel(x, y, src):
@@ -60,4 +151,3 @@ def bilinear_kernel(x, y, src):
             else:
                 temp += kernel[i, j] * src[y + i - 1, x + j - 1]  # Calculate the result
     return temp / 16  # Return the result
-
