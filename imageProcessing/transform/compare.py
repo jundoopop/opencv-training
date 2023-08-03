70de1d99777532e7dae7e4f165e1aeea0c30df3c
jundoopop <jundolpop@gmail.com>
2023-08-04 06:07:24 +0900

    Translation Fix: - Resolve the problem which crops an image by starting point so Image is not cropped for translation properly

--- imageProcessing/transform/compare.py
+++ imageProcessing/transform/compare.py
@@ -3,16 +3,21 @@ import translation as tr
 import numpy as np
 from pathlib import Path
 
-path = "/home/treeplanter/graphics/pyOpenCv/imageProcessing/source/paris.jpg" # Path to the image
-sample1 = cv.imread(path, cv.IMREAD_GRAYSCALE) # Get a first sample: Paris
+path = "/home/treeplanter/graphics/pyOpenCv/imageProcessing/source/paris.jpg"  # Path to the image
+sample1 = cv.imread(path, cv.IMREAD_GRAYSCALE)  # Get a first sample: Paris
 
-filter = np.array([[1,0],[0,1]], np.uint8) # Filter to apply to the image (2x2 matrix)
+filter = np.array(
+    [[1, 0], [0, 1]], np.uint8
+)  # Filter to apply to the image (2x2 matrix)
 
-while cv.waitKey(20) != ord("q"): # Wait for the user to press the "q" key
-    cv.imshow("Translated - x: 0, y: 50", tr.move(sample1, 50, 100)) # x: 50, y: 100 translation
-    cv.imshow("Translated - x: 0, y: 80",tr.move(sample1, 0, 80)) # x: 0, y: 80 translation
-    cv.imshow("2 x 2 Matrix applied", cv.filter2D(sample1, -1, filter)) # Apply the filter to the image
-    
-cv.destroyAllWindows() # Close all windows
-exit(0) # Exit the program
+while cv.waitKey(20) != ord("q"):  # Wait for the user to press the "q" key
+    cv.imshow("Default Sample Image", sample1)  # x: 50, y: 100 translation
+    cv.imshow(
+        "Move1 Translated - x: 0, y: 80", tr.move(sample1, 0, 80)
+    )  # x: 0, y: 80 translation
+    cv.imshow(
+        "Move2 Translated - x: 0, y: 80", tr.move2(sample1, 0, 80)
+    )  # x: 0, y: 80 translation
 
+cv.destroyAllWindows()  # Close all windows
+exit(0)  # Exit the program
--- imageProcessing/transform/translation.py
+++ imageProcessing/transform/translation.py
@@ -1,8 +1,24 @@
 import numpy as np
 
+
 def move(img, move_x, move_y):
-    row, col = img.shape # Get the shape of the image
-    img = img[move_y:, move_x:] # Just left the part of the image that will be visible after the translation
-    newImg = np.zeros((row, col), np.uint8) # Create a new image with the same shape as the original
-    newImg[move_y:, move_x:] = img # Put the translated image in the new image
+    row, col = img.shape  # Get the shape of the image
+    newImg = np.zeros(
+        (row, col), np.uint8
+    )  # Create a new image with the same shape as the original
+    newImg[move_y:, move_x:] = img[
+        : row - move_y, : col - move_x
+    ]  # Put the translated image in the new image
     return newImg
+
+
+def move2(img, move_x, move_y):
+    row, col = img.shape  # Get the shape of the image
+
+    newImg = np.zeros(
+        (row + move_y, col + move_x), np.uint8
+    )  # Pad the image with zeros on the left and top sides
+    newImg[move_y:, move_x:] = img  # Put the original image in the padded image
+
+    # Crop the padded image to the original size
+    return newImg[:row, :col]
