b67355caffbc235765b375e28512229236d6bb4b
jundoopop <jundolpop@gmail.com>
2023-10-10 08:38:07 +0900

    [fix] firstHough.py - TBA

--- imageProcessing/detection/originalHough.py
+++ imageProcessing/detection/originalHough.py
@@ -11,18 +11,6 @@ Packages:
 import numpy as np
 import cv2 as cv
 
-# Read image
-
-# Declare parameter space.
-
-
-def parameter_space(x, y, rho, theta):
-    # Calculate rho and theta values for each point in the image.
-    rho = x * np.cos(theta) + y * np.sin(theta)
-    theta = np.deg2rad(np.arange(-90, 90, 1))
-
-    return [rho, theta]
-
 
 def hough_transform(image, rho_res=1):
     """
@@ -54,8 +42,7 @@ def hough_transform(image, rho_res=1):
     # and increment their value in the accumulator.
     # Returns the indices of the detected pixels as edges.
     edge_points = np.argwhere(edges)
-    for coordinate in edge_points:
-        y, x = coordinate
+    for y, x in edge_points:
         for theta_index, theta in enumerate(thetas):
             rho = x * np.cos(theta) + y * np.sin(theta)  # Hough Space
             rho_index = np.argmin(np.abs(rhos - rho))
@@ -65,7 +52,7 @@ def hough_transform(image, rho_res=1):
 
 
 path = "/home/treeplanter/graphics/pyOpenCv/\
-imageProcessing/source/blocks.png"  # Path to the image.
+imageProcessing/source/concreteblock.jpg"  # Path to the image.
 target = cv.imread(path)  # Load a grayscale of the image.
 if target is None:  # Check if the image was loaded.
     print("Could not read the image")
@@ -78,22 +65,27 @@ accumulator, thetas, rhos = hough_transform(target_gray)
 # Step 4: Find peaks in the accumulator
 # A peak in the accumulator represents a potential line in the image.
 # For simplicity, we're thresholding the accumulator to detect these peaks.
-hough_threshold = 120
-accumulator_peaks = np.where(accumulator > hough_threshold)
+hough_threshold = 50
+accumulate_array = np.where(accumulator > hough_threshold)
 
 cnt = 0
 # For each detected peak, draw the corresponding line on the image.
-for rho_index, theta_index in zip(*accumulator_peaks):  # type: ignore
+for rho_index, theta_index in zip(*accumulate_array):  # type: ignore
     rho = rhos[rho_index]
     theta = thetas[theta_index]
-    a = np.cos(theta)
-    b = np.sin(theta)
-    x0 = a * rho
-    y0 = b * rho
-    x1 = int(x0 + 300 * (-b))
-    y1 = int(y0 + 300 * (a))
-    x2 = int(x0 - 300 * (-b))
-    y2 = int(y0 - 300 * (a))
+    cos_theta = np.cos(theta)
+    sin_theta = np.sin(theta)
+    # x0, y0 are the coordinates of the end of rho which meets the slope.
+    x0 = cos_theta * rho
+    y0 = sin_theta * rho
+
+    # Draw a line between (x1, y1) and (x2, y2)
+    # They are each end of the slope. 
+    # 
+    x1 = int(x0 + 300 * (-sin_theta))
+    y1 = int(y0 + 300 * (cos_theta))
+    x2 = int(x0 - 300 * (-sin_theta))
+    y2 = int(y0 - 300 * (cos_theta))
     if cnt < 50:
         print(f"x1: {x1}, y1: {y1}, x2: {x2}, y2: {y2}")
         cnt += 1
